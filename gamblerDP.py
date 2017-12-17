40bc9c7b95bb4972db4f99cbd186d89794e03e65
jamesliao2016 <liaohkpolyu@foxmail.com>
2017-12-17 11:31:49 +0800

    comments

--- gamblerDP.py
+++ gamblerDP.py
@@ -1,57 +1,50 @@
+#######################################################################
+# Copyright (C)                                                       #
+# 2017 James LIAO(453143942@qq.com)                                   #
+# Permission given to modify the code as long as you keep this        #
+# declaration at the top                                              #
+#######################################################################
+
 import numpy as np
 import matplotlib.pyplot as plt
 
-# Random policy
-# Assume there is a linear grid, the chance of choosing each direction is same
-# The objective is to reach the two ends
-
-stNum = 100
+# In this program, we evaluate and improve the policy simutaneously
+stNum = 100  # The number of the possible states
+# Epsilon threshold has twofold functions:
+# 1. minimum improvement of policy improvement for each state to change state action;
+# 2. maximum improvement of entire function to end policy improvement
 epsThr = 1.0e-5
 probH = 0.4
 
-states = list(range(0,stNum+1))
+states = list(range(0,stNum+1)) # The states
+# Value function.
+# Caution: the value of state 100 is not 1, the "state 100" is the terminal state
 valVec = np.zeros(stNum+1)
-policy = [0 for ii in range(stNum+1)]
-# valVec[stNum]=1.0
-rewVec = np.zeros(stNum+1)
-rewVec[stNum] = 1.0
+policy = [0 for ii in range(stNum+1)]  # Action for each state
+rewVec = np.zeros(stNum+1)  # Reward function
+rewVec[stNum] = 1.0  # Caution: the reward is 1 only when the capital hits 100
 
 # Function for value evaluation
-
-deltaVal = 1
-tmpval = 0
 runTime = 1
-# while (deltaVal > epsThr):
 while True:
     deltaVal = 0.0
     gf=[]
     for qq in (range(1,stNum)):
         oldVal = valVec[qq]
         optValTmp = valVec[qq]
-        optSol = policy[qq]
         for ee in range(min(qq+1,stNum+1-qq)):
             futVal = probH * (rewVec[qq+ee] + valVec[qq+ee]) \
                      + (1 - probH) * (rewVec[qq-ee] + valVec[qq-ee])
-            if futVal > optValTmp+epsThr:
+            # Caution: the action changes if and only if the improvement is larger than epsThr
+            if futVal > (optValTmp + epsThr):
                 optValTmp = futVal
-                optSol = ee
                 valVec[qq] = futVal
                 policy[qq] = ee
         gg = abs(valVec[qq]-oldVal)
-        gf.append(gg)
-    if (max(gf)<epsThr) and (min(gf)<epsThr/10.0):
+        gf.append(gg)  # Record the policy improvement for each state
+    # Caution: the policy improvement ends if and only if the maximum improvement is smaller than epsThr
+    if (max(gf)<epsThr):
         break
-    # deltaVal = max(deltaVal,gg)
     runTime += 1
-# print(deltaVal)
-
-print(len(states))
-# print(len(policy))
-print('optimal policy')
-print(policy)
-
-print(valVec)
-print(runTime)
 plt.plot(states, policy)
-# plt.plot(valVec)
 plt.show()
